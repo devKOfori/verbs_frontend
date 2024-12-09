281b5b917053b141402997b00d1af76c5f16b16e
devKOfori <groupfivethree@gmail.com>
2024-12-09 12:30:49 +0200

    Create home page

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Topbar from "../components/Topbar";
+const Homepage = () => {
+  return (
+    <div>
+      <Topbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Homepage;
