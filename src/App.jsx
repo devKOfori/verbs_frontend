f083211f81581db4ea0e78f65d436c887d88161c
devKOfori <groupfivethree@gmail.com>
2024-12-09 12:33:11 +0200

    Update App component and project main styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-import Hero from './components/Hero'
+import { useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Homepage from "./pages/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
+);
 
 function App() {
-
   return (
     <>
-      <Hero/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
