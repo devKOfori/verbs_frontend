ff55af02919ffb748cc91d88a89df1fbbe98013b
devKOfori <groupfivethree@gmail.com>
2024-12-10 10:36:52 +0200

    Add validation to login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../assets/styles/Login.css"
+import "../assets/styles/Login.css";
+import { MdErrorOutline } from "react-icons/md";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [errors, setErrors] = useState({});
+
+  const validateLoginForm = (data) => {
+    const errors = {};
+    if (!data.email) {
+      errors.email = "Enter your email address to login.";
+    }
+    if (!data.password) {
+      errors.password = "Enter your password address to login.";
+    }
+    return errors;
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loginFormData = {
+      email,
+      password,
+    };
+    const loginFormErrors = validateLoginForm(loginFormData);
+    setErrors(loginFormErrors);
+    if (Object.keys(loginFormErrors).length === 0) {
+      console.log("sign in successful...");
+    }
+  };
   return (
     <div className="login-page">
       <div className="login-form-container">
@@ -18,7 +49,14 @@ const Login = () => {
                 id="email"
                 placeholder="Email"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
+              {errors.email && (
+                <div className="error-message">
+                  <MdErrorOutline />
+                  {errors.email}
+                </div>
+              )}
             </div>
             <div className="form-group mb-2x">
               <input
@@ -27,10 +65,21 @@ const Login = () => {
                 id="password"
                 placeholder="Password"
                 required
+                onChange={(e) => setPassword(e.target.value)}
               />
+              {errors.password && (
+                <div className="error-message">
+                  <MdErrorOutline />
+                  {errors.password}
+                </div>
+              )}
             </div>
             <div className="login-submit-btn">
-              <button className="btn login-form-btn mb-x" type="submit">
+              <button
+                onClick={(e) => handleLogin(e)}
+                className="btn login-form-btn mb-x"
+                type="submit"
+              >
                 Sign in
               </button>
             </div>
@@ -39,7 +88,10 @@ const Login = () => {
         <div className="login-form-links flex">
           <div className="form-link reset-password">
             Forgotten Password?
-            <Link to="register.html" className="login-form__reset-password-link">
+            <Link
+              to="register.html"
+              className="login-form__reset-password-link"
+            >
               Reset
             </Link>
           </div>
