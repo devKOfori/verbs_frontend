cffd4fb8a64c765b6f576c3a8d9287e17a9ad1e9
devKOfori <groupfivethree@gmail.com>
2024-12-14 20:06:39 +0200

    Update topbar menus

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 import { MdLogin } from "react-icons/md";
 import { AiOutlineUserAdd } from "react-icons/ai";
 import '../assets/styles/Topbar.css'
+import { IoPersonOutline } from "react-icons/io5";
+import { MdFavoriteBorder } from "react-icons/md";
+import { BsCart } from "react-icons/bs";
 
 const Topbar = () => {
   return (
     <>
       <div className="topbar">
         <div className="topbar-content flex bg-white">
-          <div className="btn login-btn">
+          {/* <div className="btn login-btn">
             <span className="login-text"><Link to="/login" className="link-btn">login</Link></span>{" "}
             <span className="login-icon">
               <MdLogin />
@@ -20,6 +23,15 @@ const Topbar = () => {
             <span className="signup-icon">
               <AiOutlineUserAdd />
             </span>
+          </div> */}
+          <div className="topbar-icon user-icon">
+          <Link to="/login" className="link-btn"><IoPersonOutline/></Link>
+          </div>
+          <div className="topbar-icon favorites-icon">
+          <Link to="/" className="link-btn"><MdFavoriteBorder /></Link>
+          </div>
+          <div className="topbar-icon cart-icon">
+            <Link to="/" className="link-btn"><BsCart /></Link>
           </div>
         </div>
       </div>
