ca10b56448dadf4c097816ae75a77146aa852ceb
devKOfori <groupfivethree@gmail.com>
2024-12-15 10:20:36 +0200

    Create landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../assets/styles/Hero.css'
+import sample from '../assets/images/frame_sample.jpg'
 const Hero = () => {
   return (
     <>
       <div className="hero">
-
+        <div className="coming-soon-container">
+          <img src={sample} alt="not found" />
+          <h1 className="hero-headline">Coming Soon.</h1>
+          {/* <p className="hero-subheadline">Thoughtful decor to inspire your everyday</p> */}
+        </div>
       </div>
     </>
     
