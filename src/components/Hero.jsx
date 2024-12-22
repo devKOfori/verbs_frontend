fe7cd2cb493e2889a16ec1e5b1ae16668b5a7ab4
devKOfori <groupfivethree@gmail.com>
2024-12-22 22:15:22 +0200

    Update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import '../assets/styles/Hero.css'
-import sample from '../assets/images/frame_sample.jpg'
+import React from "react";
+import "../assets/styles/Hero.css";
+import sample from "../assets/images/frame_sample.jpg";
+import sample2 from "../assets/images/sample2.jpeg";
+import sample3 from "../assets/images/sample.jpeg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Hero = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <>
       <div className="hero">
-        <div className="coming-soon-container">
-          <img src={sample} alt="not found" />
-          <h1 className="hero-headline">Coming Soon.</h1>
-          {/* <p className="hero-subheadline">Thoughtful decor to inspire your everyday</p> */}
+        <div className="hero-content">
+          <div className="hero-headline">
+            <h1>New Thoughts</h1>
+          </div>
+          <div className="hero-showcase">
+            <div className="hero-showcase-items">
+              <Slider {...settings}>
+              <div className="hero-showcase-item">
+                <img src={sample} alt="" />
+              </div>
+              <div className="hero-showcase-item">
+                <img src={sample2} alt="" />
+              </div>
+              <div className="hero-showcase-item">
+                <img src={sample3} alt="" height="400" />
+              </div>
+              </Slider>
+            </div>
+          </div>
         </div>
       </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
